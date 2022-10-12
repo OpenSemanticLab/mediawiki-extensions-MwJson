7284c02b495f4bd4e99baa95e4c201b360569c1a
SimonTaurus <simon.stier@gmx.de>
2022-10-12 05:46:13 +0000

    add uuid

--- modules/ext.MwJson.util/MwJson_util.js
+++ modules/ext.MwJson.util/MwJson_util.js
@@ -6,6 +6,16 @@ mwjson.util = class {
 		return (performance.now().toString(36) + Math.random().toString(36)).replace(/\./g, "");
 	}
 
+	static uuidv4() {
+        return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+            (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        );
+    }
+
+	static OslId() {
+        return "OSL" + mwjson.util.uuidv4().replaceAll("-","");
+    }
+
 	static valueIfExists(value, default_value = "") {
 		if (value) return value;
 		else return default_value;
