df118dfdfa9eda89f4a314d04cd48cdf12e4cddd
Simontaurus <simon.stier@gmx.de>
2024-11-03 20:35:28 +0100

    fix: expose wgMwJsonAiCompletionApiUrl in mw.config

--- includes/MwJson.php
+++ includes/MwJson.php
@@ -13,6 +13,7 @@ class MwJson {
 	// see https://www.mediawiki.org/wiki/Manual:Hooks/ResourceLoaderGetConfigVars
 	public static function onResourceLoaderGetConfigVars( array &$vars, $skin, Config $config ): void {
 		$vars['wgMwJsonAllowSubmitInvalide'] = $config->get( 'MwJsonAllowSubmitInvalide' );
+		$vars['wgMwJsonAiCompletionApiUrl'] = $config->get( 'MwJsonAiCompletionApiUrl' );
 	}
 
 }
